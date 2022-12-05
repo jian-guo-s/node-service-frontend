8fecd2a7a289bd7e544a7df1ab00c786dfd9d7df
leary423 <leary620@163.com>
2022-12-05 17:49:04 +0800

    del wallets css

--- frontend/src/components/Wallets.vue
+++ frontend/src/components/Wallets.vue
@@ -38,3 +38,23 @@ const onClickConnect = () => {
 defineExpose({onClickConnect})
 </script>
 
+<!-- <style>
+main {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+button {
+  border: none;
+  border-radius: 6px;
+  background: hsla(160, 100%, 37%, 1);
+  color: white;
+  padding: 14px 12px;
+  margin-top: 40px;
+  font-family: inherit;
+  font-weight: 600;
+  font-size: 16px;
+  cursor: pointer;
+}
+</style> -->
